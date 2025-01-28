b36b58f0af941748e678e31ad1690b9d2ef35d64
void-the-droid <bhuwan626282@gmail.com>
2025-01-28 20:14:34 +0545

    routing exampe

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { Routes,Route } from "react-router";
+import { Routes,Route,Link } from "react-router";
 import { About } from "./About";
 import { Contact } from "./contact";
 import {Home} from "./Home"
+import "./App.css"
 
 export default function App() {
   return (
 <>
+
+<div className=" flex flex-row justify-center m-5 ">
+<div className="floatingNav ">
+<Link to="/">  Home </Link>
+<Link to="/about"> About </Link>
+<Link to="/contact"> Contact </Link>
+</div></div>
+
 <h1 className=" text-center text-3xl font-bold  p-5">Here is the tailwind & Reatrouter check</h1>
+
+{/* Routes create wrapper for wraping different route */}
+
  <Routes>
   <Route path="/" element={<Home/>} /> 
   <Route path="/about" element={<About/>} />
